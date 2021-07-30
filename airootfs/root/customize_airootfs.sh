e89c245785745f28eae1b65cd80b06613f00c686
yoshi314 <yoshi314@gmail.com>
2021-07-30 20:16:13 +0200

    add customizations

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -7,6 +7,7 @@ set -e -u
 # Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.
 
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(pl_PL\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
